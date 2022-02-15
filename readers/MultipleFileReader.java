adb106ab4efab9f38e3531a82013589f6c810893
Prathamesh206 <98940544+Prathamesh206@users.noreply.github.com>
2022-02-15 16:12:21 +0530

    Update MultipleFileReader.java

--- readers/MultipleFileReader.java
+++ readers/MultipleFileReader.java
@@ -6,16 +6,16 @@ import org.apache.log4j.Logger;
 
 public class MultipleFileReader {
 	final Logger log=Logger.getLogger(MultipleFileReader.class);               //create a logger object for print the statement
-	SingleFileReader singleFileReader=new SingleFileReader();
+	SingleFileReader singleFileReader=new SingleFileReader();                  //create a SingleFileReader Object
 	public  void multiFileRead(String path) {                                   
 		try {
 			                              
 			File file = new File(path);                                     //give the location of the file
 			File[] files=file.listFiles();			                       //storing all the files in the directory in file array
 			         
-			for(File File2:files) {                                        //itearting each file into file object                           
+			for(File singleFile:files) {                                        //itearting each file into file object                           
 				if(File2.getName().endsWith(".txt")==true) {               //if the file ends with txt          
-					String newPath=path+File2.getName();                   //then all the file stored into the newPath    
+					String newPath=path+singleFile.getName();                   //then all the file stored into the newPath    
 					singleFileReader.singleFileRead(newPath);              //Creating SingleFileReader object and read the file in the array
 				}
 			}
