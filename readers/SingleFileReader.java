1c26658fdbcc8b6cec3cf58a880ce586f6d36655
Prathamesh206 <98940544+Prathamesh206@users.noreply.github.com>
2022-02-17 10:31:20 +0530

    Update SingleFileReader.java

--- readers/SingleFileReader.java
+++ readers/SingleFileReader.java
@@ -8,7 +8,7 @@ public class SingleFileReader {
 	final Logger log=Logger.getLogger(MultipleFileReader.class);          //Create a logger object for  print the statement                                                     
 	public void singleFileRead()  {
 		UserInput userInput=new UserInput();                           //creating a UserInput object for input
-		ArrayList<String> fileObject=userInput.SingleFileInput();        //call the SingleFileInput method for taking the input from user and storing into the arraylist
+		ArrayList<String> fileObject=userInput.singleFileInput();        //call the SingleFileInput method for taking the input from user and storing into the arraylist
 		FileWordsCount word=new FileWordsCount();                         //with the help of the wordCount object call the wordscount method for counting the number of words in the file
 		word.wordsCount(fileObject);                                //count the no of word present in the file
 	}
